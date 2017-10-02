e52a1fc58cfddb4dbbe381f3b8cfeff75ab6f8bd
abiyasa <ilham.suhardi@gmail.com>
2017-10-02 16:07:31 +0200

    Upgrade to marko 4 & use lasso-marko

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,10 @@
-var template = require('marko').load(require.resolve('./template.marko'));
+var template = require('./index.marko');
 
 module.exports = function(req, res) {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
     var renderMode = req.query.renderMode || 'progressive-out-of-order';
-    var jsLocation = req.query.jsLocation || 'middle';
-
     var viewModel = {
-        renderMode: renderMode,
-        jsLocation: jsLocation,
-        widgetConfig: {
-            renderMode: renderMode,
-            jsLocation: jsLocation
-        }
+        renderMode: renderMode
     };
 
-    template.render(viewModel, res);
-
+    res.marko(template, viewModel);
 };
